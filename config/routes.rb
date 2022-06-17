f2386c82e5e0115943b291098baa3257cc0f4bdc
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-06-17 09:15:33 +0300

    Add Recipes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  resources :recipes
   devise_for :users
   devise_scope :user do
     authenticated :user do
-      root "home#index", as: :authenticated_root
+      root "recipes#index", as: :authenticated_root
     end
 
     unauthenticated do
