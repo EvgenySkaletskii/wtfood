9d1ca9e539c11e72d4b2de6090ecf30658cbcce8
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-07-17 19:20:24 +0300

    Add tests for user model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,15 @@
 require "rails_helper"
 
 RSpec.describe User, type: :model do
-  # before(:each) do
-  #   @user = create(:user)
-  # end
-
   describe "validations" do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:email) }
     it { should validate_presence_of(:password) }
+  end
+
+  describe "relations" do
+    it { should have_many(:recipes) }
+    it { should have_many(:comments) }
+    it { should have_many(:likes) }
   end
 end
