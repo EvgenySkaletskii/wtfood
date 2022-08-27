86b91600a8e41588ca59cca067aa2ce82e799b20
Evgeny Skaletskii <evgeny.skaletskii@jetruby.com>
2022-08-27 22:06:12 +0300

    Add layout_by_resource method

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,18 @@
 class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
+  layout :layout_by_resource
 
   protected
 
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+  end
+
+  def layout_by_resource
+    if devise_controller? && action_name != "edit"
+      "devise"
+    else
+      "application"
+    end
   end
 end
